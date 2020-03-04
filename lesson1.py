639a34ece9e9574fe0d74416e7236bfa622291df
Alexey Kumantsov <kumantsov50@gmail.com>
2020-03-04 17:21:50 +0300

    1.1

--- lesson1.py
+++ lesson1.py
@@ -1,1 +1,12 @@
 # Урок первый
+def seconds_to_time(seconds):
+    seconds = int(seconds)
+    ss = seconds % 60
+    mm = (seconds // 60) % 60
+    hh = seconds // 3600
+    return "%02d:%02d:%02d" % (hh, mm, ss)
+
+
+input_seconds = input("Введите кол-во секунд: ")
+
+print(seconds_to_time(input_seconds))
